e1d952558b324c98f4f23d0d55da29a467745486
Al-Amin Islam Hridoy <36563890+HRIDOY-BUZZ@users.noreply.github.com>
2024-07-30 08:39:51 +0600

    Update main.php

--- crawler/main.php
+++ crawler/main.php
@@ -11,43 +11,43 @@
 
 START:
 
-clear_console();
-//Display Intro
-echo constyle("\n\t\tSHOPIFY WEB MULTI-CRAWLER\n\n", 96);
+    clear_console();
+    //Display Intro
+    echo constyle("\n\t\tSHOPIFY WEB MULTI-CRAWLER\n\n", 96);
 
-// Display MENU
-echo constyle(constyle("\n\t\t\t[MENU]:\n\n", 96), 4);
-echo constyle(
-    "\t\t1.\tRun Full Crawler\n".
-    "\t\t2.\tRun Part-1 Only\n".
-    "\t\t3.\tRun Part-2 Only\n".
-    "\t\t0.\tExit\n\n", 
-93);
+    // Display MENU
+    echo constyle(constyle("\n\t\t\t[MENU]:\n\n", 96), 4);
+    echo constyle(
+        "\t\t1.\tRun Full Crawler\n".
+        "\t\t2.\tRun Part-1 Only\n".
+        "\t\t3.\tRun Part-2 Only\n".
+        "\t\t0.\tExit\n\n", 
+    93);
 
 INPUT:
-// Get user input
-echo constyle("\t\tEnter your choice: ", 92);
-$choice = get_single_char();
-echo $choice . "\n";
+    // Get user input
+    echo constyle("\t\tEnter your choice: ", 92);
+    $choice = get_single_char();
+    echo $choice . "\n";
 
-// Perform action based on user input
-switch ($choice) {
-    case '1':
-        clear_console();
-        part1(); part2(); pause();
-        goto START; break;
-    case '2':
-        clear_console();
-        part1(); pause();
-        goto START; break;
-    case '3':
-        clear_console();
-        part2(); pause();
-        goto START; break;
-    case '0':
-        exit;
-    default:
-        echo "\t\tInvalid choice. Please try again.\n\n";
-        goto INPUT; break;
-}
+    // Perform action based on user input
+    switch ($choice) {
+        case '1':
+            clear_console();
+            part1(); part2(); pause();
+            goto START; break;
+        case '2':
+            clear_console();
+            part1(); pause();
+            goto START; break;
+        case '3':
+            clear_console();
+            part2(); pause();
+            goto START; break;
+        case '0':
+            exit;
+        default:
+            echo "\t\tInvalid choice. Please try again.\n\n";
+            goto INPUT; break;
+    }
 ?>
